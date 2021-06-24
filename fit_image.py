eff2c1e4ff1e60c83fc9b33881cb94612d23fce8
tova <webmaster@tomvarga.de>
2021-06-24 18:43:24 +0200

    Working Code to pass the assignment, but needs comments and adjust the paths to default ones

---
+++ fit_image.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+import shutil
+
+directory = '/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/images'
+
+for file in os.listdir(directory):
+    if file == ".DS_Store":
+        continue
+    filename = file.split(".")
+    img = Image.open('/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/images/' + file)
+    target_name = filename[0] + ".jpg"
+    rgb_image = img.rotate(270).resize((128,128)).convert('RGB')
+    rgb_image.save('/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/src/' + target_name)
+    print("Converted image saved as " + target_name)
+
+
