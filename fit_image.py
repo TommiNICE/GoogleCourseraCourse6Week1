9e91f21c907e7dcfefa4c6d6b3ad4b4a827bdb5f
t0v4 <tomislavvarga37@gmail.com>
2021-06-24 18:49:05 +0200

    Inserted default paths

--- fit_image.py
+++ fit_image.py
@@ -3,13 +3,13 @@
 from PIL import Image
 import os
 
-directory = '/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/images'
+directory = '/path/to/image/folder/'
 
 for file in os.listdir(directory):
     if file == ".DS_Store":
         continue
     filename = file.split(".")
-    img = Image.open('/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/images/' + file)
+    img = Image.open('/path/to/image/folder/' + file)
     target_name = filename[0] + ".jpg"
     rgb_image = img.rotate(270).resize((128,128)).convert('RGB')
     rgb_image.save('/home/tova/Documents/PycharmProjects/GoogleCourseraCourse6Week1/src/' + target_name)
